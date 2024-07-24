27c3ade02f7625a8dc4b31a1a4f5478b54b763be
gusttavo.miguel.silva@gmail.com <you@example.com>
2024-07-24 18:59:55 -0300

    Refatoração da classe Credentials

--- monitoringIncidentsTrato/src/main/java/com/example/botTelegram/utis/serviceaideData/Credentials.java
+++ monitoringIncidentsTrato/src/main/java/com/example/botTelegram/utis/serviceaideData/Credentials.java
@@ -1,16 +1,16 @@
 package com.example.botTelegram.utis.serviceaideData;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class Credentials {
-    private final String CSM_APP_URL = "https://csm3.serviceaide.com";
-    private final String USER_AUTH_TOKEN = "apiusertokenslicetoken40285";
-    private final String SLICE_TOKEN = "VbxiZZ1IpsGe5T7L_l.t-Ho68629aCgt";
-    private final String WEBSERVICE_USER_NAME = "wsIncidentes@serviceaide.trato";
-    private final String WEBSERVICE_USER_PASSWORD = "vD7@6!Hcx1";
-    private final String API_URL_PATH = "https://csm3.serviceaide.com/csmconnector/Ticket";
-    private final String FILTER = "?filter=((NonTranslatedTicketStatus ne 'Archive' " +
+    private static final String CSM_APP_URL = "https://csm3.serviceaide.com";
+    private static final String USER_AUTH_TOKEN = "apiusertokenslicetoken40285";
+    private static final String SLICE_TOKEN = "VbxiZZ1IpsGe5T7L_l.t-Ho68629aCgt";
+    private static final String WEBSERVICE_USER_NAME = "wsIncidentes@serviceaide.trato";
+    private static final String WEBSERVICE_USER_PASSWORD = "vD7@6!Hcx1";
+    private static final String API_URL_PATH = "https://csm3.serviceaide.com/csmconnector/Ticket";
+    private static final String FILTER = "?filter=((NonTranslatedTicketStatus ne 'Archive' " +
             "and NonTranslatedTicketStatus ne 'Request - Delete' " +
             "and NonTranslatedTicketStatus ne 'Closed' or (NonTranslatedTicketStatus eq 'Closed' " +
             "and LastModTimestamp gt 1716433200)) " +
@@ -18,5 +18,29 @@ public class Credentials {
             "and (((AssignedGroupID eq '37' or AssignedGroupID eq '38') " +
             "and (NonTranslatedTicketStatus eq 'New' or NonTranslatedTicketStatus eq 'Active' or NonTranslatedTicketStatus eq 'Queued') " +
             "and (TypeName eq 'incident') and (TicketAgingRange eq '0-2 days'))))";
-    private final String API_URL = API_URL_PATH + FILTER;
+    private static final String API_URL = API_URL_PATH + FILTER;
+
+    public String getCSM_APP_URL() {
+        return CSM_APP_URL;
+    }
+
+    public String getUSER_AUTH_TOKEN() {
+        return USER_AUTH_TOKEN;
+    }
+
+    public String getSLICE_TOKEN() {
+        return SLICE_TOKEN;
+    }
+
+    public String getWEBSERVICE_USER_NAME() {
+        return WEBSERVICE_USER_NAME;
+    }
+
+    public String getWEBSERVICE_USER_PASSWORD() {
+        return WEBSERVICE_USER_PASSWORD;
+    }
+
+    public String getAPI_URL() {
+        return API_URL;
+    }
 }
