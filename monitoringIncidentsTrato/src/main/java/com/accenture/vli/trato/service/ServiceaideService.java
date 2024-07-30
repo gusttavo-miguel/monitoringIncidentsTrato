1ad1c5b86d24a43baea1734c4c281d721f57e968
gusttavo.miguel.silva@gmail.com <you@example.com>
2024-07-29 22:21:37 -0300

    ajuste

--- monitoringIncidentsTrato/src/main/java/com/accenture/vli/trato/service/ServiceaideService.java
+++ monitoringIncidentsTrato/src/main/java/com/accenture/vli/trato/service/ServiceaideService.java
@@ -7,14 +7,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-//import java.net.URI;
-//import java.net.URLEncoder;
-//import java.net.http.HttpClient;
-//import java.net.http.HttpRequest;
-//import java.net.http.HttpResponse;
-//import java.net.http.HttpHeaders;
-//import java.io.IOException;
-//import java.nio.charset.StandardCharsets;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class ServiceaideService {
 
@@ -45,9 +44,8 @@ public class ServiceaideService {
 //
 //        // Cria a requisição com o header
 //        HttpRequest request = HttpRequest.newBuilder()
-////                .uri(URI.create(serviceaideData.getAPI_URL()))
 //                .uri(URI.create(serviceaideData.getAPI_URL_PATH().concat(filter)))
-//                .header("Content-Type", "application/json")
+////                .header("Content-Type", "application/json")
 //                .header("csm_app_url", serviceaideData.getCSM_APP_URL())
 //                .header("user_auth_token", serviceaideData.getUSER_AUTH_TOKEN())
 //                .header("slice_token", serviceaideData.getSLICE_TOKEN())
@@ -66,7 +64,5 @@ public class ServiceaideService {
 //
 //        assert response != null;
 //        return response.body();
-//    }
-
     }
 }
