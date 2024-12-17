3d5ac9ea51f7b5bbe2364b5c8d5d2c26720806aa
Gustavo Miguel <77859657+gusttavo-miguel@users.noreply.github.com>
2024-12-16 22:28:43 -0300

    Início da migração

---
+++ monitoringIncidentsTrato/src/main/java/com/accenture/vli/trato/MonitoringIncidentsTrato.java
@@ -0,0 +1,12 @@
+package com.accenture.vli.trato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringIncidentsTrato {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringIncidentsTrato.class, args);
+    }
+}
